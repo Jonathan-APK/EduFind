15f5670f772fc67610ef875032cfe09f294c3bed
Heng Ze Hao <facebookhzhcongo@hotmail.com>
2017-09-27 23:51:14 +0800

    Edits - Updated content_main to display homepage.
    Things to do
    - Need to figure out it what I did is correct (didn't touch main_ui.xml at all)

--- app/src/main/java/com/example/utsav/edufind/MainUI.java
+++ app/src/main/java/com/example/utsav/edufind/MainUI.java
@@ -18,17 +18,6 @@ public class MainUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         // This is just a sample usage of the function
         DistanceCalculation d1 = new DistanceCalculation();
