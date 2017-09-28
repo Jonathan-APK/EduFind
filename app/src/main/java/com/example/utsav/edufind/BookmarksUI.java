50a318ec147d3c07745492f213d75500dda54e05
Heng Ze Hao <facebookhzhcongo@hotmail.com>
2017-09-28 23:32:31 +0800

    Edits - Created Toolbar for bookmarks and new_search_ui

--- app/src/main/java/com/example/utsav/edufind/BookmarksUI.java
+++ app/src/main/java/com/example/utsav/edufind/BookmarksUI.java
@@ -2,6 +2,7 @@ package com.example.utsav.edufind;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by boonleng94 on 27/9/2017.
@@ -13,5 +14,9 @@ public class BookmarksUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bookmarks_ui);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Bookmarks");
     }
 }
--- app/src/main/java/com/example/utsav/edufind/NewSearchUI.java
+++ app/src/main/java/com/example/utsav/edufind/NewSearchUI.java
@@ -2,6 +2,7 @@ package com.example.utsav.edufind;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class NewSearchUI extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class NewSearchUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_search_ui);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("New Search");
     }
 }
