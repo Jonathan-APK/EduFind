86dcbd0b05b903422486c940c63c4a5c5ccbb287
braveducky <darkskycloud@hotmail.com>
2017-09-29 16:39:42 +0800

    hi

--- app/src/main/java/controller/CourseController.java
+++ app/src/main/java/controller/CourseController.java
@@ -9,8 +9,25 @@ import entity.PolytechnicCourse;
  */
 
 public class CourseController {
+
     public static ArrayList<PolytechnicCourse> retrieveListOfPolyCourses() {
-        ArrayList<PolytechnicCourse> list = new ArrayList<PolytechnicCourse>();
+        ArrayList<PolytechnicCourse> polyList = new ArrayList<PolytechnicCourse>();
+
+        //call ur IO shit here
+
+        return list;
+    }
+
+    public static ArrayList<UniversityCourse> retrieveListOfUniCourses() {
+        ArrayList<UniversityCourse> uniList = new ArrayList<UniversityCourse>();
+
+        //call ur IO shit here
+
+        return list;
+    }
+
+    public static ArrayList<Institution> retrieveListOfInstitution() {
+        ArrayList<Institution> schList = new ArrayList<Institution>();
 
         //call ur IO shit here
 
