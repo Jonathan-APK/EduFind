e91caf42810ee730ab214977ee7a4cfba5ce2a0e
braveducky <darkskycloud@hotmail.com>
2017-09-30 01:41:52 +0800

    hi

--- app/src/main/java/controller/CourseController.java
+++ app/src/main/java/controller/CourseController.java
@@ -15,16 +15,23 @@ import entity.Institution;
 
 public class CourseController {
 
+    private static Institution institute;
+    private static String csvFile;
+    private static BufferedReader br = null;
+    private static String line;
+    private static String delimiter = "^";
+    private static String[] tempArray;
+    private static ArrayList<UniversityCourse> uniList;
+    private static ArrayList<PolytechnicCourse> polyList;
+    private static PolytechnicCourse poly;
+    private static UniversityCourse uni;
+
     public static ArrayList<PolytechnicCourse> retrieveListOfPolyCourses() {
 
-        ArrayList<PolytechnicCourse> polyList = new ArrayList<PolytechnicCourse>();
-        PolytechnicCourse poly = new PolytechnicCourse();
-        Institution institute  = new Institution();
+        polyList = new ArrayList<PolytechnicCourse>();
+        poly = new PolytechnicCourse();
 
-        String csvFile = "/Users/darks/Desktop/poly.csv";
-        BufferedReader br = null;
-        String line = "";
-        String delimiter = "^";
+        csvFile = "/Users/darks/Desktop/poly.csv";
 
         try {
 
@@ -32,18 +39,20 @@ public class CourseController {
             while ((line = br.readLine()) != null) {
 
                 // use comma as separator
-                String[] tempPoly = line.split(delimiter);
+                tempArray = line.split(delimiter);
 
-                poly.setInstitution(tempPoly[0]);
-                poly.setSchool(tempPoly[0]);
-                poly.setCourseName(tempPoly[0]);
-                poly.setInterest(tempPoly[0]);
-                poly.setSpecialization(tempPoly[0]);
-                poly.setL1R4(tempPoly[0]);
-                poly.setEducationLevel(tempPoly[0]);
-                poly.setIntake(tempPoly[0]);
-                poly.setWebsite(tempPoly[0]);
-                poly.setCourseDescription(tempPoly[0]);
+                institute  = new Institution(tempArray[0],tempArray[12],Integer.parseInt(tempArray[5]));
+
+                poly.setInstitution(institute);
+                poly.setSchool(tempArray[1]);
+                poly.setCourseName(tempArray[2]);
+                poly.setInterest(tempArray[3]);
+                poly.setSpecialization(tempArray[4]);
+                poly.setL1R4(Integer.parseInt(tempArray[6]));
+                poly.setEducationLevel(tempArray[7]);
+                poly.setIntake(Integer.parseInt(tempArray[8]));
+                poly.setWebsite(tempArray[9]);
+                poly.setCourseDescription(tempArray[10]);
 
                 polyList.add(poly);
             }
@@ -61,22 +70,122 @@ public class CourseController {
                 }
             }
         }
-        return list;
+        return polyList;
     }
 
     public static ArrayList<UniversityCourse> retrieveListOfUniCourses() {
-        ArrayList<UniversityCourse> uniList = new ArrayList<UniversityCourse>();
 
-        //call ur IO shit here
+        uniList = new ArrayList<UniversityCourse>();
+        uni = new UniversityCourse();
 
-        return list;
+        csvFile = "/Users/darks/Desktop/uni.csv";
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                tempArray = line.split(delimiter);
+
+                institute  = new Institution(tempArray[0],tempArray[12],Integer.parseInt(tempArray[5]));
+
+                uni.setInstitution(institute);
+                uni.setSchool(tempArray[1]);
+                uni.setCourseName(tempArray[2]);
+                uni.setInterest(tempArray[3]);
+                uni.setSpecialization(tempArray[4]);
+                uni.setGradePointAverage(Integer.parseInt(tempArray[6]));
+                uni.setEducationLevel(tempArray[7]);
+                uni.setIntake(Integer.parseInt(tempArray[8]));
+                uni.setWebsite(tempArray[9]);
+                uni.setCourseDescription(tempArray[10]);
+
+                uniList.add(uni);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return uniList;
     }
 
     public static ArrayList<Institution> retrieveListOfInstitution() {
+
         ArrayList<Institution> schList = new ArrayList<Institution>();
+        uniList = retrieveListOfUniCourses();
+        polyList = retrieveListOfPolyCourses();
+        boolean alreadyAdded = false;
 
-        //call ur IO shit here
+        for (int i = 0; i < uniList.size(); i++) {
 
-        return list;
+            if(schList.isEmpty()){
+
+                institute  = uniList.get(i).getInstitution();
+                schList.add(institute);
+
+            }
+            else{
+
+                for(int j=0; j< schList.size();j++){
+
+                    if(uniList.get(i).getInstitution().getInstitution().equals(schList.get(j).getInstitution())){
+                        alreadyAdded = true;
+                        break;
+                    }
+
+                }
+
+                if(alreadyAdded = false){
+                    schList.add(institute);
+                }
+                else
+                    alreadyAdded = false;
+
+            }
+
+        }
+
+
+        for (int i = 0; i < polyList.size(); i++) {
+
+            if(schList.isEmpty()){
+
+                institute  = polyList.get(i).getInstitution();
+                schList.add(institute);
+
+            }
+            else{
+
+                for(int j=0; j< schList.size();j++){
+
+                    if(polyList.get(i).getInstitution().getInstitution().equals(schList.get(j).getInstitution())){
+                        alreadyAdded = true;
+                        break;
+                    }
+
+                }
+
+                if(alreadyAdded = false){
+                    schList.add(institute);
+                }
+                else
+                    alreadyAdded = false;
+
+            }
+
+        }
+
+        return schList;
     }
 }
